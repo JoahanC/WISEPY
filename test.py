037acdb6c7a3bfe863d7fe99dbcdc1bd6044b222
Joahan Castaneda Jaimes <54558611+JoahanC@users.noreply.github.com>
2022-06-17 11:05:28 -0700

    Deleting obsolete file.

--- test.py
+++
@@ -1,7 +0,0 @@
-from MPC_WISEcat_comparer import *
-
-
-mpc_data = '161989.txt'
-wise_data = 'table_irsa_catalog_search_results-2.csv'
-mpc_data, wise_data, new_epochs = comparison(mpc_data, wise_data)
-print(new_epochs)
