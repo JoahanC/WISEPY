492b27e759d8a83d65bf82fbc46e5d168639002a
JoahanC <joahan.castanedajaimes@gmail.com>
2022-06-21 10:27:50 -0700

    Added query string printout

--- mpc_wise_comparer.py
+++ mpc_wise_comparer.py
@@ -5,12 +5,9 @@ import sys
 new_epochs = comparer(sys.argv[1], sys.argv[2], False)
 visualizer(sys.argv[1], sys.argv[2], sys.argv[3])
 
-print("All new Source Ids:")
-sid_string = ""
+print("GATOR source id query string:")
+sid_string = "source_id in ("
 counter = 0
 for epoch in new_epochs:
-    print(new_epochs[epoch])
-    counter += 1
-    if counter <= 50:
-        sid_string += "'" + new_epochs[epoch][0] + "', "
-print(sid_string[:-2])
+    sid_string += "'" + new_epochs[epoch][0] + "', "
+print(sid_string[:-2] + ')')
