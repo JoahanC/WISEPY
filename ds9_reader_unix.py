76d64cbd158185323ffdc11974cf1631ef02fce1
JoahanC <joahan.castanedajaimes@gmail.com>
2022-06-23 15:44:03 -0700

    Refactored ds9 reader code

--- ds9_reader_unix.py
+++ ds9_reader_unix.py
@@ -5,6 +5,13 @@ import re
 
 
 def make_region(file, w_band, source_ids):
+    """
+    Makes a region file for a given .fits file in the regions folder.
+    Arguments: file (str) -- the .fits file
+               w_band (str) -- the corresponding w band for the file
+               source_ids (dict) -- the source_id lookup table
+    Returns: None (file in regions folder)
+    """
     sid = file[:9:]
     with open(f"regions/{sid}_{w_band}.reg", 'w') as file:
         file.write("# Region file format: DS9 version 4.1\n")
@@ -16,89 +23,91 @@ def make_region(file, w_band, source_ids):
         file.write(f'circle({source_ids[sid][0]},{source_ids[sid][1]},15.000")')
 
 
+def data_sort(source_ids, new_epochs):
+    """
+    The sorting algorithm for a given object's dataset.
+    """
+    
+    files = os.listdir("WISE_Files/")
+    to_run = []
+    for file in files:
+        if file[:9] in source_ids:
+            to_run.append(file)
+
+    file_id = {}
+    for file in to_run:
+        if file == ".DS_Store" or file == ".DS_S":
+            pass
+        if int(file[:5]) in file_id.keys():
+            file_id[int(file[:5])].append(file)
+        else: 
+            file_id[int(file[:5])] = [file]
+
+    key_sort = list(file_id.keys())
+    key_sort.sort()
+
+    number_sorted = {}
+    for key in key_sort:
+        for file in to_run:
+            code = int(file[0:5])
+            if code == key and code in number_sorted.keys():
+                number_sorted[code].append(file)
+            elif code == key and code not in number_sorted.keys():
+                number_sorted[code] = [file]
+            else:
+                pass
+
+    for number in number_sorted:
+        regex = r"[0-9]{5}[a]"
+        matches = []
+        for test in number_sorted[number]:
+            if len(re.findall(regex, test)) == 0:
+                pass
+            else:
+                matches.append(test)
+
+        regex = r"[0-9]{5}[b]"
+        for test in number_sorted[number]:
+            if len(re.findall(regex, test)) == 0:
+                pass
+            else:
+                matches.append(test)
+
+        number_sorted[number] = matches
+
+    # Putting sorted files into run list
+    sorted_run = []
+    for number in number_sorted:
+        for file in number_sorted[number]:
+            sorted_run.append(file)
+
+    # W band sort
+    idx = list(range(len(sorted_run)))[::2]
+
+    w_sorted = []
+    for i in idx:
+        pair = (sorted_run[i], sorted_run[i + 1])
+        if pair[0][10:12] == "w1":
+            w_sorted.extend([pair[0], pair[1]])
+        else:
+            w_sorted.extend([pair[1], pair[0]])
+
+    # Renaming files to correct directory
+    renamed_sorted_run = []
+    for file in w_sorted:
+        renamed_sorted_run.append("WISE_Files/" + file)
+
+    return renamed_sorted_run
+
+
 mpc_file = "161989.txt"
 wise_file = "table_irsa_catalog_search_results.tbl"
 new_epochs = comparer(mpc_file, wise_file, True)
-
 source_ids = []
 for epoch in new_epochs:
     source_ids.append(new_epochs[epoch][0][:9])
-
-files = os.listdir("WISE_Files/")
-to_run = []
-for file in files:
-    if file[:9] in source_ids:
-        to_run.append(file)
-
-# Generating list of sorted 5 numbers keys
-file_id = {}
-for file in to_run:
-    if file == ".DS_Store" or file == ".DS_S":
-        pass
-    if int(file[:5]) in file_id.keys():
-        file_id[int(file[:5])].append(file)
-    else: 
-        file_id[int(file[:5])] = [file]
-
-key_sort = list(file_id.keys())
-key_sort.sort()
-#print(key_sort)
-
-# Sorting all files by first five numebers
-number_sorted = {}
-for key in key_sort:
-    for file in to_run:
-        code = int(file[0:5])
-        if code == key and code in number_sorted.keys():
-            number_sorted[code].append(file)
-        elif code == key and code not in number_sorted.keys():
-            number_sorted[code] = [file]
-        else:
-            pass
-
-# SORT BY LETTER PIECEWISE
-for number in number_sorted:
-    regex = r"[0-9]{5}[a]"
-    matches = []
-    for test in number_sorted[number]:
-        if len(re.findall(regex, test)) == 0:
-            pass
-        else:
-            matches.append(test)
-
-    regex = r"[0-9]{5}[b]"
-    for test in number_sorted[number]:
-        if len(re.findall(regex, test)) == 0:
-            pass
-        else:
-            matches.append(test)
-
-    number_sorted[number] = matches
-
-# Putting sorted files into run list
-sorted_run = []
-for number in number_sorted:
-    for file in number_sorted[number]:
-        sorted_run.append(file)
-
-# W band sort
-idx = list(range(len(sorted_run)))[::2]
-
-w_sorted = []
-for i in idx:
-    pair = (sorted_run[i], sorted_run[i + 1])
-    if pair[0][10:12] == "w1":
-        w_sorted.extend([pair[0], pair[1]])
-    else:
-        w_sorted.extend([pair[1], pair[0]])
-
-#for file in w_sorted:
-#    print(file)
-
-# Renaming files to correct directory
-renamed_sorted_run = []
-for file in w_sorted:
-    renamed_sorted_run.append("WISE_Files/" + file)
+    
+renamed_sorted_run = data_sort(source_ids, new_epochs)
 
 run_string = "ds9 -scale log -tile "
 for file in renamed_sorted_run[:50]:
