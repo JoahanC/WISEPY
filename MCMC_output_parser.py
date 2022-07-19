3cb4ca508dacbf7f74c368854575ee9310c5b2ad
JoahanC <joahan.castanedajaimes@gmail.com>
2022-07-19 11:41:46 -0700

    Updated parser script

--- MCMC_output_parser.py
+++ MCMC_output_parser.py
@@ -19,6 +19,7 @@ def julian_days_utc_converter(jd):
     Returns: (tup) -- a utc format entry with year[0], month[1], and
                       day[2].
     """
+
     jd_adjusted = float(jd) + 0.5
     decimal_day = modf(jd_adjusted)[0]
     x_2 = floor(jd - 1721119.5)
@@ -35,6 +36,27 @@ def julian_days_utc_converter(jd):
     return year, month, day + decimal_day
 
 
+def retrieve_output_data():
+    
+    output_file = open("PJDFC.out")
+    output_file.readline()
+
+    diameters = []
+    chis = []
+    gammas = []
+
+    for line in output_file.readlines():
+        datum = line.strip().split()
+        if len(datum) < 10:
+            #capture odd cases where col 6 was runing into col 5.  Only a handful, so just skip 
+            continue
+    
+        diameters.append(np.e ** float(datum[5]))
+        chis.append(float(datum[8]))
+        gammas.append(np.e ** float(datum[4]))
+
+    return diameters, chis, gammas
+
 
 def retrieve_MCMC_data():
     #cp, not mv, so that there is a record of the actual outputs  
@@ -45,7 +67,7 @@ def retrieve_MCMC_data():
     #^*^ parse and plot SED_data.out here 
     # best fit solution used for SED
     SED_file = open("SED_data.out")
-    print("\nReading in best fit solution used for SED")
+    print("Reading in best fit solution used for SED.")
     datum_minor = SED_file.readline().rstrip().split()
     datum_major = SED_file.readline().rstrip().split()
     best_fit = {}
@@ -100,13 +122,20 @@ def retrieve_MCMC_data():
     data_x = []
     data_y = []
     data_y_error = []
+    first_line = True
 
     while True:
         line = SED_file.readline()
         
+        if "SRGB" not in line and first_line:
+            color.append("#444444")
+            data_x.append([])
+            data_y.append([])
+            data_y_error.append([])
+
         if line == "":
             break
-        
+
         if "/ft" in line:
             esed.append([])
             line = SED_file.readline()
@@ -146,7 +175,7 @@ def retrieve_MCMC_data():
 
     datelabels = []
     cshfile = os.popen("ls run*.csh").readline().rstrip()
-    print("\n***Using "+cshfile+" to get MJDs. Make sure this is right***\n")
+    print("\n*** Using "+cshfile+" to get MJDs. Make sure this is right*** \n")
     cshlines = open(cshfile)
     for line in cshlines.readlines():
         epoch_data = line.rstrip().split(',')
@@ -176,7 +205,7 @@ def generate_SED_plot(esed, wavelengths, datelabels, data_x, data_y, data_y_erro
     Returns: None, generates two plots titled best_fit_SED.pdf/png
     """
 
-    print("Generating best fit plot for SED")
+    print("Generating best fit plot for SED.")
     
     colors = ["#000000", "#ff0000", "#0000ff", "#dd00dd", "#ee7700", 
         "#00ee77", "#999999", "#cccc55", "#55cccc", "#cc55cc"]
@@ -196,30 +225,32 @@ def generate_SED_plot(esed, wavelengths, datelabels, data_x, data_y, data_y_erro
         if max(esed[i]) > y_high:
             y_high = max(esed[i])
 
+        # Plot flux for each wavelength
         plt.plot(wavelengths, esed[i], 
                 color=colors[i % 10], 
                 ls=linestyles[i % 3], 
                 label=datelabels[i])    
 
-        ax.set_xscale("log")
-        ax.set_yscale("log")
-
+        # Adjust y values and set up error ranges
         lower_errors = []
         higher_errors = []
         data_y_adjusted=[]
-
         for j in range(len(data_y[i])):
-            
+
+            lower_errors.append(data_y[i][j] * (1 - 1 / (10 ** (data_y_error[i][j] / 2.5))))
+            higher_errors.append(data_y[i][j] * (10 ** (data_y_error[i][j] / 2.5) - 1))
+
             if data_y_error[i][j] > 0:    
                 data_y_adjusted.append(data_y[i][j])
 
             else:
-                #measured flux is negative, so plot at an artifically low point, and make error bars correct
+                # Measured flux is negative, so plot at an artifically
+                # low point, and make error bars correct
                 data_y_adjusted.append(1e-99)
 
-            lower_errors.append(data_y[i][j] * (1 - 1 / (10 ** (data_y_error[i][j] / 2.5))))
-            higher_errors.append(data_y[i][j] * (10 ** (data_y_error[i][j] / 2.5) - 1))
-            
+        # Set scale and include error bars
+        ax.set_xscale("log")
+        ax.set_yscale("log")
         plt.errorbar(data_x[i], data_y_adjusted, 
                     yerr=[lower_errors, higher_errors],
                     ecolor=colors[i%10],
@@ -243,6 +274,7 @@ def generate_diameter_vs_albedo_plot():
     Returns: None, generates two plots titled diameter_vs_albedo.png/pdf
     """
 
+    print("Generating diameter vs albedo plots.")
     diameters = []
     albedos = []
     
@@ -301,6 +333,7 @@ def generate_diameter_histogram():
     Returns: None, generates two plots titled diameter_histogram.png/pdf
     """
 
+    print("Generating diameter histogram.")
     diameters = []
     data_file = open("DvsAlb.dat")
     for line in data_file.readlines():
@@ -339,7 +372,7 @@ def generate_diameter_histogram():
     plt.axvline(sigma_2_high,color='#cc0000',ls='dotted')
     
     # Labeling plot
-    plt.xlabel("Log diameter (km)")
+    plt.xlabel("Diameter (km)")
     plt.ylabel("Number of Monte Carlo Results")
     plt.savefig("diameter_histogram.png")
     plt.savefig("diameter_histogram.pdf")
@@ -354,6 +387,7 @@ def generate_diameter_vs_period_plot():
     Returns: None, generates two plots titled diameter_vs_period.png/pdf
     """
 
+    print("Generating diameter vs period plots.")
     diameters = []
     periods = []
     data_file = open("DvsPeriod.dat")
@@ -363,13 +397,16 @@ def generate_diameter_vs_period_plot():
         periods.append(float(datum[1]))
 
     if max(periods) - min(periods) == 0:
-        print("Fixed period used, skipping D vs Period plot")
+        print("Fixed period used, skipping diameter vs period plot")
 
     else:
-        # Set up base figure
-        plt.figure(figsize=[6, 6])    
-        plt.scatter(diameters, periods, s=1.5, c="black", marker='o', edgecolors='')
-        
+        # Generate base figure
+        plt.figure(figsize=[6,6])
+        ax = plt.gca()
+        plt.scatter(diameters, periods, s=1,marker='o',color='k')
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+
         # Set up bins for contour levels
         H_bins, x_bins, y_bins = np.histogram2d(diameters, periods, bins=20)
         x_bins_adjusted = [(x_bins[i + 1] - x_bins[i]) / 2. + x_bins[i] for i in range(len(x_bins) - 1)]
@@ -377,27 +414,27 @@ def generate_diameter_vs_period_plot():
         H_bins_adjusted  = np.transpose(H_bins)
         H_output = np.amax(H_bins_adjusted)
         
-        H_bins_adjusted = list(H_bins_adjusted.flatten())
-        H_bins_adjusted.sort()
-        H_total = sum(H_bins_adjusted)
+        H_bins_flat = list(H_bins_adjusted.flatten())
+        H_bins_flat.sort()
+        H_total = sum(H_bins_flat)
 
         H_cumulative = 0
         level_1 =- 1
         level_2 =- 1
-        for i in range(len(H_bins_adjusted)):
-            H_cumulative += H_bins_adjusted[i]
+        for i in range(len(H_bins_flat)):
+            H_cumulative += H_bins_flat[i]
             if H_cumulative > 0.05 * H_total and level_2 < 0:
                 level_2 = i
             if H_cumulative > 0.32 * H_total and level_1 < 0:
                 level_1 = i
-        
+
         # Add contour lines to plot
         plt.contour(x_bins_adjusted, y_bins_adjusted, H_bins_adjusted, 
                     colors="#cc0000", 
-                    levels=[H_bins_adjusted[level_2], H_bins_adjusted[level_1]])
+                    levels=[H_bins_flat[level_2], H_bins_flat[level_1]])
         plt.text(np.mean(diameters), 0.95 * max(periods), 
                 "Contours contain 68%\nand 95.5% of all points")
-        plt.ylim(-2, 0)
+
         plt.xlabel("Diameter (km)")
         plt.ylabel("Rotation period")
         plt.savefig("diameter_vs_period.png")
@@ -405,17 +442,146 @@ def generate_diameter_vs_period_plot():
         plt.close()
 
 
-if sys.version[0] != '3':
-    print("Please run with python3")
-    exit()
+def generate_diameter_vs_gamma_plot(diameters, gammas):
+    """
+    Generates a plot of diameter and gamma solutions for the MCMC output.
 
-best_fit, best_fit_plotters, epoch_condition, wavelengths = retrieve_MCMC_data()
-generate_SED_plot(best_fit_plotters[0], best_fit_plotters[1], 
-                  best_fit_plotters[2], best_fit_plotters[3], 
-                  best_fit_plotters[4], best_fit_plotters[5])
-generate_diameter_histogram()
-generate_diameter_vs_albedo_plot()
-generate_diameter_vs_period_plot()
+    Arguments: diameters (list) -- a series of diameter solutions
+               gammas (list) -- a series of gamma solutions
+
+    Returns: None, generates two plots titled diameter_vs_gamma.png/pdf
+    """
+    print("Generating diameter vs gamma plots.")
+    plt.figure(figsize=[6,6])
+    ax = plt.gca()
+    plt.scatter(diameters, gammas, s=1, marker='o', color='k')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.xlabel("Diameter (km)")
+    plt.ylabel("Gamma")
+    plt.savefig("diameter_vs_gamma.png")
+    plt.savefig("diameter_vs_gamma.pdf")
+    plt.close()
+
+
+def generate_diameter_vs_chi_plots(diameters, chis):
+    """
+    Generates a plot of diameter and chi solutions for the MCMC output.
+
+    Arguments: diameters (list) -- a series of diameter solutions
+               chis (list) -- a series of chi solutions
+
+    Returns: None, generates two plots titled diameter_vs_chi.png/pdf
+             and two zoomed plots titled diameter_vs_chi_zoom.png/pdf
+    """
+
+    print("Generating diameter vs chi plots.")
+    plt.figure(figsize=[6, 6])
+    plt.subplots_adjust(left=0.15, right=0.95, top=0.95)
+    plt.loglog(diameters, chis, color='k', marker='o', ms=0.5, ls="None")
+    plt.xlabel("Diameter (km)", fontsize=13)
+    plt.ylabel(r"fit $\chi^2$", fontsize=13)
+    plt.savefig("diameter_vs_chi.png")
+    plt.savefig("diameter_vs_chi.pdf")
+    plt.close()
+
+    plt.figure(figsize=[6, 6])
+    plt.subplots_adjust(left=0.15, right=0.95, top=0.95)
+    plt.plot(diameters, chis, color='k', marker='o', ms=0.5, ls='None')
+    plt.xlabel("Diameter (km)", fontsize=13)
+    plt.ylabel(r"fit $\chi^2$", fontsize=13)
+    plt.ylim(0.9 * min(chis), 2 * min(chis))  
+    plt.savefig("diameter_vs_chi_zoom.png")
+    plt.savefig("diameter_vs_chi_zoom.pdf")
+    plt.close()
+
+def generate_gamma_vs_chi_plots(gammas, chis):
+    """
+    Generates a plot of gamma and chi solutions for the MCMC output.
+
+    Arguments: gammas (list) -- a series of gamma solutions
+               chis (list) -- a series of chi solutions
+
+    Returns: None, generates two plots titled gamma_vs_chi.png/pdf
+    """
+
+    print("Generating gamma vs chi plots.")
+    plt.figure(figsize=[6, 6])
+    plt.subplots_adjust(left=0.15, right=0.95, top=0.95)
+    plt.loglog(gammas, chis, color='k', marker='o', ms=0.5, ls='None')
+    plt.xlabel(r"Thermal inertia ($J~m^{-2}~s^{-0.5}~K^{-1})$)", fontsize=13)
+    plt.ylabel(r"fit $\chi^2$", fontsize=13)
+
+    plt.savefig("gamma_vs_chi.png")
+    plt.savefig("gamma_vs_chi.pdf")
+    plt.close()
+
+
+def display_MCMC_results():
+    with open("best_fit.txt", 'r') as file:
+        line_1 = file.readline().split()
+        line_1[4] = "I=1 ... MNC ="
+        output_1 = f"{line_1[0]} {line_1[1]} {line_1[2]} {line_1[3]}"
+        output_1 += f" {line_1[4]} {line_1[5]} {line_1[6]}s"
+        line_2 = file.readline().split()
+        output_2 = f"Patch & Total Weights: {line_2[4]} & {line_2[5]}"
+        print(output_2)
+        line_3 = file.readline().strip()
+        print(line_3)
+        line_4 = file.readline().strip()
+        print(line_4)
+        file.readline()
+        print("Out of 1 ... NMC loop\n\n*** Properties ***\n")
+        line_6 = file.readline().split()
+        output_6 = f"Diameter (Mean): {line_6[1][0:6]} +/- {line_6[2]}"
+        print(output_6)
+        output_7 = f"Diameter (Median): {line_6[4][0:6]} +{line_6[5][0:4]}%/-{line_6[6]}"
+        print(output_7)
+        line_8 = file.readline().split()
+        output_8 = f"p_V (Mean): {line_8[2]} +/- {line_8[4]}"
+        print(output_8)
+        output_9 = f"p_V (Median): {line_8[6][0:6]} +/-n{line_8[8][0:4]}"
+        print(output_9)
+        line_10 = file.readline().split()
+        output_10 = f"Theta_1 (Mean): {line_10[1][0:6]} +/- {line_10[2]}"
+        print(output_10)
+        output_11 = f"Theta_1 (Median): {line_10[4][0:6]} +{line_10[5][0:4]}%/-{line_10[6]}"
+        print(output_11)
+        line_12 = file.readline().split()
+        if len(line_12) == 6:
+            output_12 = f"Period (hour): {line_12[3][0:5]} +{line_12[4]}%/-{line_12[5]}"
+        if len(line_12) == 5:
+            output_12 = f"Period (hour): {line_12[3][0:4]} +{line_12[3][5:]}%/{line_12[4]}"
+        print(output_12)
+        line_13 = file.readline().split()
+        if len(line_13) == 2:
+            output_13 = f"Sqrt(Kappa*Rho*C): {line_13[1][0:5]} +{line_13[2]}&/{line_13[3]}"
+        print(output_13)
+        line_14 = file.readline().split()
+        output_14 = f"Crater Fraction: {line_14[2][0:5]} +{line_14[2][6:11]}/-{line_14[2][12:]}"
+        print(output_14)
+        line_15 = file.readline().split()
+        output_15 = f"p_IR/p_V: {line_15[1][0:5]} +{line_15[1][6:10]}%/-{line_15[1][11:]}"
+        print(output_15)
+        line_16 = file.readline().split()
+        output_16 = f"Pole peak at: {line_16[4]} {line_16[5]}"
+        print(output_16)
+        line_17 = file.readline().split()
+        output_17 = f"Mean pole at: {line_17[4]} {line_17[5]}"
+        print(output_17)
+        line_18 = file.readline().split()
+        output_18 = f"Moment eigenvector: {line_18[2]} at RA,DEC: "
+        output_18 += f"{line_18[5]} {line_18[6]}"
+        print(output_18)
+        line_19 = file.readline().split()
+        output_19 = f"Moment eigenvector: {line_19[2]} at RA,DEC: "
+        output_19 += f"{line_19[5]} {line_19[6]}"
+        print(output_19)
+        line_20 = file.readline().split()
+        output_20 = f"Moment eigenvector: {line_20[2]} at RA,DEC: "
+        output_20 += f"{line_20[5]} {line_20[6]}\n"
+        print(output_20)
+
 
 print("Echoing relevant files\n")
 os.system("echo 'PJDFC.out' | ../read-WISE-rc-MCMC-PJDFC") 
@@ -423,3 +589,16 @@ os.system("/bin/cp fort.2 Dhist.dat")
 os.system("/bin/cp fort.32 Dhist_fine.dat")
 os.system("/bin/cp fort.3 DvsPeriod.dat")
 os.system("/bin/cp fort.4 DvsAlb.dat")
+#display_MCMC_results()
+
+best_fit, best_fit_plotters, epoch_condition, wavelengths = retrieve_MCMC_data()
+diameters, chis, gammas = retrieve_output_data()
+generate_SED_plot(best_fit_plotters[0], best_fit_plotters[1], 
+                  best_fit_plotters[2], best_fit_plotters[3], 
+                  best_fit_plotters[4], best_fit_plotters[5])
+generate_diameter_histogram()
+generate_diameter_vs_albedo_plot()
+generate_diameter_vs_period_plot()
+generate_diameter_vs_gamma_plot(diameters, gammas)
+generate_diameter_vs_chi_plots(diameters, chis)
+generate_gamma_vs_chi_plots(gammas, chis)
